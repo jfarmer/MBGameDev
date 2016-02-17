48a6bd859a208cac7992b655f4d3eec74b7b4049
MiLeung <michaeleleung@gmail.com>
2016-02-16 21:50:52 -0800

    add arrow input

--- state0.js
+++ state0.js
@@ -1,8 +1,19 @@
 var demo = {};
+var hero;
 demo.state0 = function(){};
 
 demo.state0.prototype = {
-    preload: function(){},
-    create: function(){},
-    update: function(){}
+    preload: function(){
+        game.load.image('hero', 'assets/sprites/hero.png');
+    },
+    create: function(){
+        game.stage.backgroundColor = '#00FF00';
+        hero = game.add.sprite(0, 0, 'hero');
+        hero.scale.setTo(0.5, 0.5);
+    },
+    update: function(){
+        if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            hero.x = hero.x + 2;
+        }
+    }
 };
